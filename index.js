9bd1ddac6b5dd91ab49cc71e66777765b48a8614
pvavrik <pavelv@salsitasoft.com>
2016-07-15 15:46:35 +0200

    Indent

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var Nightmare = require('nightmare');
 var nightmare = Nightmare({webPreferences: {nodeIntegration: true}});
 
 nightmare
-  .viewport(1000,960)
-  .goto('http://www.beastrestaurant.co.uk')
-  .evaluate(function () {
-  	return document.querySelector('html').outerHTML;
-  })
-  .end()
-  .then(function(html) {
-  	return console.log(html);
-  });
+.viewport(1000,960)
+.goto('http://www.beastrestaurant.co.uk')
+.evaluate(function () {
+	return document.querySelector('html').outerHTML;
+})
+.end()
+.then(function(html) {
+	return console.log(html);
+});
