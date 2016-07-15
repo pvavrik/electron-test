f8ef4545f5c77065667f80fec169799863ad23a2
pvavrik <pavelv@salsitasoft.com>
2016-07-15 15:20:44 +0200

    init using Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: xvfb-run node index.js
---
+++ index.js
@@ -0,0 +1,14 @@
+var Nightmare = require('nightmare');
+
+var nightmare = Nightmare({webPreferences: {nodeIntegration: true}});
+
+nightmare
+  .viewport(1000,960)
+  .goto('http://www.beastrestaurant.co.uk')
+  .evaluate(function () {
+  	return document.querySelector('html').outerHTML;
+  })
+  .end()
+  .then(function(html) {
+  	return console.log(html);
+  });
